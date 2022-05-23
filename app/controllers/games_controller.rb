f4360aba6f07ee3fd0bf0408b19ea45d626dc0f8
Jack Moran <jvckmorvn@gmail.com>
2022-05-23 16:34:52 +0100

    Challenge completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters_grid = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    answer = params[:answer]
+    letters_grid = params[:letters_grid]
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    serialized = URI.open(url).read
+    valid = JSON.parse(serialized)["found"]
+    on_grid = answer.downcase.chars.all? { |letter| answer.count(letter) <= letters_grid.downcase.count(letter) }
+    if valid && on_grid
+      @result = "Congratulations, \"#{answer}\" is a valid word!"
+    elsif valid
+      @result = "Sorry, \"#{answer}\" is not on the grid."
+    else
+      @result = "Sorry, \"#{answer}\" is not in the English dictionary."
+    end
+  end
+end
